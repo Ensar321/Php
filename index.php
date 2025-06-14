6c348d204743718d35269322a0b43110c27213a0
Ensar321 <ensarpushkolii12@gmail.com>
2025-06-14 14:38:12 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+
+$num = 4;
+$Num = 5;
+$_test = 7;
+$_Test = 3;
+//4la = 3;
+
+$school = "Digital School";
+
+echo "I love $school <br>";
+
+var_dump($school);
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+$array = ["e1","e2","e3" ];
+$array2 = array ("e1","e2","e3");
+
+$test = false;
+
+
+$x=120;
+$y=50;
+
+echo $x + $y . "<br>";
+echo $x - $y . "<br>";
+echo $x * $y . "<br>";
+echo $x / $y . "<br>";
+echo $x % $y . "<br>";
+
+
+$a = "Digital ";
+$b = "School";
+
+$c = $a . $b;
+echo $c;
+echo "<br>";
+$the_string = "Digital School";
+echo strlen($the_string);
+
+echo "<br>";
+
+echo str_word_count($the_string);
+
+$programming = "Programing is not cool";
+echo str_replace("not","very", $programming);
+
+echo "<br>";
+
+echo strrev($programming);
